e54123a2b350a2be02da1df7f9e22fee4aee3f1a
Edwin Kofler <edwin@kofler.dev>
2021-09-08 23:10:37 -0700

    fix: Fix load.bash so it actually works

--- load.bash
+++ load.bash
@@ -1,21 +1,12 @@
 # shellcheck shell=bash
 
-# TODO: autogen by basalt?
-
+# TODO: autogen by basalt
 if [ -z "$BASALT_PACKAGE_PATH" ]; then
 	if [ "${BASH_SOURCE[0]::1}" = / ]; then
-		BASALT_PACKAGE_PATH="${BASH_SOURCE[0]}"
-	elif command -v greadlink &>/dev/null; then
-		BASALT_PACKAGE_PATH="$(greadlink -f "${BASH_SOURCE[0]}")"
-	elif command -v realfile &>/dev/null; then
-		BASALT_PACKAGE_PATH="$(realfile "${BASH_SOURCE[0]}")"
-	elif command -v readlink &>/dev/null; then
-		BASALT_PACKAGE_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
+		BASALT_PACKAGE_PATH="${BASH_SOURCE[0]%/*}"
+	else
+		BASALT_PACKAGE_PATH="$(CDPATH=; cd "${BASH_SOURCE[0]%/*}" &>/dev/null; printf "$PWD")"
 	fi
-
-	BASALT_PACKAGE_PATH="${BASALT_PACKAGE_PATH%/*}"
-	BASALT_PACKAGE_PATH="${BASALT_PACKAGE_PATH%/*}"
-	BASALT_PACKAGE_PATH="${BASALT_PACKAGE_PATH%/*}"
 fi
 
 for f in "$BASALT_PACKAGE_PATH"/pkg/lib/{,source/,util/}?*.sh; do
